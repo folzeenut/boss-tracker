ed3e65f2db14cf14e3419430e7e2a4ae0f712002
folzeenut <folzeenut@gmail.com>
2025-12-29 00:04:13 +0800

    Add files via upload

---
+++ bosses.js
@@ -0,0 +1,39 @@
+module.exports = [
+  // Short Cycle Bosses (10-21h)
+  { name: "Venatus", level: 60, cycle: 10, cycleType: "short" },
+  { name: "Viorent", level: 65, cycle: 10, cycleType: "short" },
+  { name: "Ego", level: 70, cycle: 21, cycleType: "short" },
+
+  // Long Cycle Bosses (24-48h)
+  { name: "Lady Dalia", level: 83, cycle: 18, cycleType: "long" },
+  { name: "Araneo", level: 83, cycle: 24, cycleType: "long" },
+  { name: "Undomiel", level: 85, cycle: 24, cycleType: "long" },
+  { name: "Livera", level: 90, cycle: 24, cycleType: "long" },
+  { name: "General Aquleus", level: 85, cycle: 29, cycleType: "long" },
+  { name: "Baron Baraudmore", level: 88, cycle: 32, cycleType: "long" },
+  { name: "Amentis", level: 88, cycle: 29, cycleType: "long" },
+  { name: "Larba", level: 98, cycle: 35, cycleType: "long" },
+  { name: "Gareth", level: 98, cycle: 32, cycleType: "long" },
+  { name: "Titore", level: 98, cycle: 37, cycleType: "long" },
+  { name: "Wannitas", level: 93, cycle: 48, cycleType: "long" },
+  { name: "Metus", level: 93, cycle: 48, cycleType: "long" },
+  { name: "Duplican", level: 93, cycle: 48, cycleType: "long" },
+  { name: "Shuliar", level: 95, cycle: 35, cycleType: "long" },
+  { name: "Catena", level: 100, cycle: 35, cycleType: "long" },
+  { name: "Secreta", level: 100, cycle: 62, cycleType: "long" },
+  { name: "Ordo", level: 100, cycle: 62, cycleType: "long" },
+  { name: "Asta", level: 100, cycle: 62, cycleType: "long" },
+  { name: "Supore", level: 100, cycle: 62, cycleType: "long" },
+
+  // Scheduled Bosses (fixed times)
+  { name: "Clemantis", level: 85, schedule: "Mon 11:30, Thu 19:00", cycleType: "scheduled" },
+  { name: "Saphirus", level: 90, schedule: "Sun 17:00, Tue 11:30", cycleType: "scheduled" },
+  { name: "Neutro", level: 88, schedule: "Tue 19:00, Thu 11:30", cycleType: "scheduled" },
+  { name: "Thymele", level: 85, schedule: "Mon 19:00, Wed 11:30", cycleType: "scheduled" },
+  { name: "Milavy", level: 90, schedule: "Sat 15:00", cycleType: "scheduled" },
+  { name: "Ringor", level: 90, schedule: "Sat 17:00", cycleType: "scheduled" },
+  { name: "Roderick", level: 92, schedule: "Fri 19:00", cycleType: "scheduled" },
+  { name: "Auraq", level: 100, schedule: "Fri 22:00, Wed 21:00", cycleType: "scheduled" },
+  { name: "Chaiflock", level: 95, schedule: "Sat 22:00", cycleType: "scheduled" },
+  { name: "Benji", level: 87, schedule: "Sun 21:00", cycleType: "scheduled" }
+];
---
+++ killlog.js
@@ -0,0 +1,1 @@
+const killLog = require("../data/killLog");
